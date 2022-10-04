3c42c16aca4ecf7229d50ba327c4f8a23ef66885
madbreach <45674132+madbreach@users.noreply.github.com>
2022-10-03 21:03:23 -0600

    Update Game.java

--- asteroid/Game.java
+++ asteroid/Game.java
@@ -1,9 +1,6 @@
 
 /*
-CLASS: Game
-DESCRIPTION: A painted canvas in its own window, updated every tenth second.
-USAGE: Extended by Asteroids.
-NOTE: You don't need to understand the details here, no fiddling neccessary.
+Game - A painted canvas in its own window, updated every tenth second.
 Original code by Dan Leyzberg and Art Simon
  */
 import java.awt.*;
